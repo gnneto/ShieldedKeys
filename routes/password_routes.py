6f341062ef71bf88914f0380dae7472d2198dc8c
gnneto <guilhermeacacio3@gmail.com>
2024-06-09 15:09:25 -0300

    ATT

--- routes/password_routes.py
+++ routes/password_routes.py
@@ -69,6 +69,42 @@ def exibirSenha(password_id):
         return jsonify({'mensagem': 'Frase de segurança incorreta'}), 403
     
 
+
+
+@passwordRoutes.route('/editarSenha/<int:password_id>', methods=['POST'])
+def editarSenha(password_id):
+    password = Password.query.get(password_id)
+    if password:
+        site_name = request.form['siteName']
+        username = request.form['username']
+        password_plain = request.form['password']
+        security_phrase = request.form['securityPhrase']
+        url = request.form['url']
+        phrase_hash = bcrypt.hashpw(security_phrase.encode(), bcrypt.gensalt())
+
+        user_id = session.get('user_id')
+        user = User.query.get(user_id)
+
+        telefone = user.numero_telefone
+        senha_hash = user.password_hash
+        password_base64 = base64.b64encode(password_plain.encode()).decode()
+        combined_info = telefone + password_base64 + senha_hash
+        combined_base64 = base64.b64encode(combined_info.encode()).decode()
+
+        password.site_name = site_name
+        password.username = username
+        password.password_hash = combined_base64
+        password.security_phrase = phrase_hash
+        password.url = url
+
+        db.session.commit()
+        return jsonify({'mensagem': 'Senha atualizada com sucesso'}), 200
+    else:
+        return jsonify({'mensagem': 'Senha não encontrada'}), 404
+
+
+
+
 @passwordRoutes.route('/deletarSenha/<int:password_id>')
 def deletarSenha(password_id):
     password = Password.query.get(password_id)
